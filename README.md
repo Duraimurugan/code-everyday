c8f4fa53013a07a9ebe4406134d2823cfe7a02a4
Rajendran <Duraimurugan.X.Rajendran.-ND@disney.com>
2020-06-14 21:50:52 -0400

    day 1: fizzbuzz problem

---
+++ README.md
@@ -0,0 +1,3 @@
+Goal is to code algorithms, puzzles, interview questions in JS/TS every day for the next 100 days
+
+Day 1: fizzbuzz problem
---
+++ fizzbuzz/fizzbuzz.ts
@@ -0,0 +1,25 @@
+/**
+ * Day 0
+ * Write a program that prints the integers from 1 to 100 (inclusive).
+ * But:
+ *  - for multiples of three, print Fizz (instead of the number)
+ *  - for multiples of five, print Buzz (instead of the number)
+ *  - for multiples of both three and five, print FizzBuzz (instead of the number)
+ */
+
+for (var number=1; number<=100; number++) {
+  const isFizz: boolean = number % 3 === 0;
+  const isBuzz: boolean = number % 5 === 0;
+  const isFizzBuzz: boolean = (number % 3 === 0) && (number % 5 === 0);
+  let result: string;
+  if (isFizzBuzz) {
+    result = 'FizzBuzz';
+  } else if(isFizz) {
+    result = 'Fizz';
+  } else if (isBuzz) {
+    result = 'Buzz';
+  }  else {
+    result = number.toString();
+  }
+  console.log(result);
+}
